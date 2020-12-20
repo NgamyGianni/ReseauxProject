46327cda95844943d7f6daa0f33515757cf23860
Gianni-Alessandro ngamy <gianningamy@MacBook-Pro-de-Gianni-Alessandro.local>
2020-12-20 13:30:35 +0100

    ignore les lignes de textes entre lignes et trames

--- lab.py
+++ lab.py
@@ -4,20 +4,30 @@ L = list()
 for line in f:
 	L.extend(line.split("\t"))
 
-#print([L[i] for i in range(len(L)) if i%17==0])
-#rint(L)
+# valide le format des offset de la trame
+def formatValideOffset(x):
+	return x[:2] == "0x" and formatValideByte(x[2:])
+
+# Valide le format des octets de la trame
+def formatValideByte(x):
+	# str -> bool
+	if len(x) != 2:
+		return False
+
+	for e in x:
+		if not("a" <= e.lower() <= "f" or "0" <= e <= "9"):
+			return False
+
+	return True
+
+
+# Construit une liste composée de lignes du fichier si l'offset est valide
 LL = list()
 for e in L:
-	LL.append(e.split())
-
-
-"""
-print(int(LL[1][0], base=16))
-print(LL[0][1:int(LL[1][0], base=16)+1])
-"""
+	if formatValideOffset(e[0:4]):
+		LL.append(e.split())
 
 # Créer la structure générale des trames : créer une liste composée de listes et chaque liste est une trame
-
 def LtoLL(L):
 	LL = []
 	tmp = []
@@ -25,6 +35,7 @@ def LtoLL(L):
 		if L[i][0] == "0x00" or i == len(L)-1:
 			LL.append(tmp)
 			tmp = []
+
 		if i < len(L)-1:
 			tmp.extend(L[i][:int(L[i+1][0], base=16)-int(L[i][0], base=16)+1])
 		else:
@@ -35,19 +46,19 @@ def LtoLL(L):
 
 #print(LtoLL(LL))
 
-#for e in LL:
-#	print(e)
-
-# Retire les indices de LL
+# Retire les offset de LL : ne garde que les formats valides des octets de la trame
 def LLtoLLclean(LL):
+	res = [[]]
 	tmp = []
 	for i in range(len(LL)):
 		for j in range(len(LL[i])):
-			if len(LL[i][j]) > 2 and LL[i][j][:2] == "0x":
-				tmp.append((i, j))
-	for e in tmp:
-		i, j = e
-		del LL[i][j]
-	return LL
+			if formatValideByte(LL[i][j]):
+				tmp.append(LL[i][j])
+		res.append(tmp)
+		tmp = []
+	
+	del res[0]
+	return res
 
 print(LLtoLLclean(LtoLL(LL)))
+print(len(LLtoLLclean(LtoLL(LL))))
